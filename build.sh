cc4f08ef38ec9208f4ec937d01bd54bbc39c4d4f
natamox <natamoxc@gmail.com>
2024-05-02 18:59:04 +0800

    build shell

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t natamox/py-minio-upload:$version . --load
+#!/bin/bash
+
+VERSION='1.4.2'
+
+version_main=$(echo $VERSION | cut -d. -f-2)
+
+version_minor=$(echo $VERSION | cut -d. -f3)
+
+if [ $version_main = '1.4' ]; then
+  ((version_minor++))
+fi
+
+NEW_VERSION="${version_main}.${version_minor}"
+
+IMAGE_TAG="natamox/py-minio-upload:${NEW_VERSION}"
+
+echo "Final tag is: $IMAGE_TAG"
+
+docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_TAG . --load
